8428cd6811f92f0dc40b07b14e6068f49814e0a7
Antoine <frama.service@openaliasbox.org>
2017-07-20 16:09:05 +0200

    fix access to DB

--- pages/users/login_post.php
+++ pages/users/login_post.php
@@ -1,66 +1,67 @@
 <?php
-    // Vérification du remplissage de tous les champs
-    if (!(isset($_POST['username']) and isset($_POST['password'])))
+require_once('./../../globalsettings.php');
+// Vérification du remplissage de tous les champs
+if (!(isset($_POST['username']) and isset($_POST['password'])))
+{
+    // echo '<p> valeurs : ' . $username . ' | ' . $email . ' | ' . $password . '</p>';
+    header('Location: connexion.php?message="Please fullfill the form"');
+}
+else
+{
+    $hostname  = $GLOBALS['hostname'];
+    $username  = $GLOBALS['username'];
+    $password  = $GLOBALS['password'];
+    $dbname    = $GLOBALS['database_name'];
+    try
     {
-        // echo '<p> valeurs : ' . $username . ' | ' . $email . ' | ' . $password . '</p>';
-        header('Location: connexion.php?message="Please fullfill the form"');
+        $bdd = new PDO("mysql:host=$hostname;dbname=$dbname;charset=utf8;port=3306",
+                        $username,
+                        $password,
+                        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+                    );
+    }
+    catch(Exception $e)
+    {
+        die('Error : '.$e->getMessage());
+    }
+
+    $username = htmlspecialchars($_POST['username']);
+
+    // Hashing password
+    $pass_hache = hash('sha256',$_POST['password']);
+
+    // Checking credentials
+    $req = $bdd->prepare('SELECT id FROM users WHERE username = :username AND password = :pass');
+    $req->execute(array(
+        'username' => $username,
+        'pass' => $pass_hache));
+
+    $resultat = $req->fetch();
+    // echo 'pass : ' . $_POST['password'] . ' / ' . $pass_hache . ' / ';
+    if (!$resultat)
+    {
+        echo 'Wrong username or password !';
+        header('Location: login.php?message=Wrong credentials');
     }
     else
     {
-        $hostname  = $GLOBALS['hostname'];
-        $username  = $GLOBALS['username'];
-        $password  = $GLOBALS['password'];
-        $dbname    = $GLOBALS['database_name'];
-        try
-        {
-            $bdd = new PDO("mysql:host=$hostname;dbname=$dbname;charset=utf8;port=3306",
-                            $username,
-                            $password,
-                            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
-                        );
-        }
-        catch(Exception $e)
-        {
-            die('Error : '.$e->getMessage());
-        }
+        session_start();
+        $_SESSION['id'] = $resultat['id'];
+        $_SESSION['username'] = $username;
 
-        $username = htmlspecialchars($_POST['username']);
-
-        // Hashing password
-        $pass_hache = hash('sha256',$_POST['password']);
-
-        // Checking credentials
-        $req = $bdd->prepare('SELECT id FROM users WHERE username = :username AND password = :pass');
+        // This is for getting the rights of the current user
+        $req-> closeCursor();
+        $req = $bdd->prepare('SELECT rights FROM users_rights WHERE id_user = :id_user');
         $req->execute(array(
-            'username' => $username,
-            'pass' => $pass_hache));
-
+                'id_user' => $_SESSION['id']));
         $resultat = $req->fetch();
-        // echo 'pass : ' . $_POST['password'] . ' / ' . $pass_hache . ' / ';
-        if (!$resultat)
-        {
-            echo 'Wrong username or password !';
-            header('Location: login.php?message=Wrong credentials');
-        }
-        else
-        {
-            session_start();
-            $_SESSION['id'] = $resultat['id'];
-            $_SESSION['username'] = $username;
+        $_SESSION['right'] = $resultat['rights'];
+        $req->closeCursor();
 
-            // This is for getting the rights of the current user
-            $req-> closeCursor();
-            $req = $bdd->prepare('SELECT rights FROM users_rights WHERE id_user = :id_user');
-            $req->execute(array(
-                    'id_user' => $_SESSION['id']));
-            $resultat = $req->fetch();
-            $_SESSION['right'] = $resultat['rights'];
-            $req->closeCursor();
-
-            // Now logged-in !
-            // echo 'You are now logged-in !';
-            // echo $username .' ' .$resultat['id'];
-            header('Location: ../../index.php?message=You are now logged-in !');
-        }
+        // Now logged-in !
+        // echo 'You are now logged-in !';
+        // echo $username .' ' .$resultat['id'];
+        header('Location: ../../index.php?message=You are now logged-in !');
     }
+}
 ?>
--- pages/users/register_post.php
+++ pages/users/register_post.php
@@ -1,4 +1,5 @@
 <?php
+require_once('./../../globalsettings.php');
 session_start();
 // Connexion à la base de données
 
