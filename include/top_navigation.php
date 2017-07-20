ba218e15a93d7d5519d13d7e725f921ad3923def
Antoine <frama.service@openaliasbox.org>
2017-07-20 16:07:18 +0200

    fix access to pic for default user

--- include/top_navigation.php
+++ include/top_navigation.php
@@ -28,31 +28,32 @@
                   <li><a href="javascript:;">Help</a></li>
                   <li><a href= <?php echo $relative_path . 'pages/users/logout.php' ?> ><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
                 </ul>
-              </li>
-              <li role="presentation" class="dropdown">
-                <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
-                  <i class="fa fa-envelope-o"></i>
-                  <!-- <span class="badge bg-green">6</span> -->
-                </a>
-                <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
-                  <li>
-                    <div class="text-center">
-                      <a>
-                        <strong>See All Alerts</strong>
-                        <i class="fa fa-angle-right"></i>
-                      </a>
-                    </div>
-                  </li>
-                </ul>
-              </li>
+                </li>
+                <li role="presentation" class="dropdown">
+                  <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+                    <i class="fa fa-envelope-o"></i>
+                    <!-- <span class="badge bg-green">6</span> -->
+                  </a>
+                  <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
+                    <li>
+                      <div class="text-center">
+                        <a>
+                          <strong>See All Alerts</strong>
+                          <i class="fa fa-angle-right"></i>
+                        </a>
+                      </div>
+                    </li>
+                  </ul>
+                </li>
             <?php
             }
             else
             {
+              echo '<li class="">';
             ?>
-              <li class="">
+
                 <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                  <img src= <?php echo $relative_path . 'resources/images/users/default.png'?> alt="">Guest
+                  <img src= <?php echo $relative_path . 'resources/users/default.png'?> alt="">Guest
                   <span class=" fa fa-angle-down"></span>
                 </a>
                 <ul class="dropdown-menu dropdown-usermenu pull-right">
