1c635a2615503696343ac291593fecac0d66af20
Antoine <frama.service@openaliasbox.org>
2017-08-15 10:21:41 +0200

    adapted sql queries to aspire db

--- sync/aspire/pushConfigs.php
+++ sync/aspire/pushConfigs.php
@@ -26,123 +26,175 @@ class ASRService
         $data = json_decode($data,true);
         $id = 1;
 
-
         $aisStmt = $this->db->stmt_init();
-        $aisProcessing = $this->db->stmt_init();
-        $canArduino  = $this->db->stmt_init();
+        $aisStmt->prepare("UPDATE config_ais SET id=?, loop_time=?;");
+        $aisStmt->bind_param(NULL, $data["config_ais"]["loopt_time"]);
+        $aisStmt->execute();
+        $aisStmt->close();
+
+        $aisProcessingStmt = $this->db->stmt_init();
+        $aisProcessingStmt->prepare("UPDATE config_ais_processing SET id=?, loopt_time=?, radius=?, mmsi_aspire=?;");
+        $aisProcessingStmt->bind_param(NULL, 
+                                        $data['config_ais_processing']['loop_time'],
+                                        $data['config_ais_processing']['radius'],
+                                        $data['config_ais_processing']['mmsi_aspire']
+                                    );
+        $aisProcessingStmt->execute();
+        $aisProcessingStmt->close();
+
+        $bufferStmt = $this->db->stmt_init();
+        $bufferStmt->prepare("UPDATE config_buffer SET id=?, compass=?, true_wind=?, windsensor=?;");
+        $bufferStmt->bind_param(NULL,
+                                $data['config_buffer']['compass'],
+                                $data['config_buffer']['true_wind'],
+                                $data['config_buffer']['windsensor']
+                            );
+        $bufferStmt->execute();
+        $bufferStmt->close();
+
+        $canArduinoStmt = $this->db->stmt_init();
+        $canArduinoStmt->prepare('UPDATE config_can_arduino SET id=?, loop_time=?;');
+        $canArduinoStmt->bind_param(NULL, $data['config_can_arduino']['loop_time']);
+        $canArduinoStmt->execute();
+        $canArduinoStmt->close();
+
         $compassStmt = $this->db->stmt_init();
+        $compassStmt->prepare('UPDATE config_compass SET id=?, loop_time=?, heading_buffer_size=?;');
+        $compassStmt->bind_param(NULL,
+                                $data['config_compass']['loop_time'],
+                                $data['config_compass']['heading_buffer_size']
+                            );
+        $compassStmt->execute();
+        $compassStmt->close();
+
         $courseRegulatorStmt = $this->db->stmt_init();
+        $courseRegulatorStmt->prepare('UPDATE config_course_regulator SET id=?, loop_time=?, max_rudder_angle=?, p_gain=?, i_gain=?, d_gain=?;');
+        $courseRegulatorStmt->bind_param(NULL,
+                                        $data['config_course_regulator']['loop_time'],
+                                        $data['config_course_regulator']['max_rudder_angle'],
+                                        $data['config_course_regulator']['p_gain'],
+                                        $data['config_course_regulator']['i_gain'],
+                                        $data['config_course_regulator']['d_gain']
+                                    );
+        $courseRegulatorStmt->execute();
+        $courseRegulatorStmt->close();
+
         $dbloggerStmt = $this->db->stmt_init();
+        $dbloggerStmt->prepare('UPDATE config_dblogger SET id=?, loop_time=?;');
+        $dbloggerStmt->bind_param(NULL, $data['config_dblogger']['loop_time']);
+        $dbloggerStmt->execute();
+        $dbloggerStmt->close();
+
         $gpsStmt = $this->db->stmt_init();
-        $httpSyncStmlt = $this->db->stmt_init();
-        $i2cStmt = $this->db->stmt_init();
+        $gpsStmt->prepare('UPDATE config_gps SET id=?, loop_time=?;');
+        $gpsStmt->bind_param(NULL, $data['config_gps']['loop_time']);
+        $gpsStmt->execute();
+        $gpsStmt->close();
+
+
+        $httpSyncStmt = $this->db->stmt_init();
+        $httpSyncStmt->prepare('UPDATE config_httpsync SET id=?, loop_time=?, remove_logs=?, push_only_latest_logs=?, boat_id=?, boat_pwd=?, srv_addr=?, configs_updated=?, route_updated=?;');
+        $httpSyncStmt->bind_param(NULL,
+                                $data['config_httpsync']['loop_time'],
+                                $data['config_httpsync']['remove_logs'],
+                                $data['config_httpsync']['push_only_latest_logs'],
+                                $data['config_httpsync']['boat_id'],
+                                $data['config_httpsync']['boat_pwd'],
+                                $data['config_httpsync']['srv_addr'],
+                                $data['config_httpsync']['configs_updated'],
+                                $data['config_httpsync']['route_updated']
+                            );
+        $httpSyncStmt->execute();
+        $httpSyncStmt->close();
+
         $lineFollowStmt = $this->db->stmt_init();
+        $lineFollowStmt->prepare('UPDATE config_line_follow SET id=?, loop_time=?;');
+        $lineFollowStmt->bind_param(NULL, $data['config_line_follow']['loop_time']);
+        $lineFollowStmt->execute();
+        $lineFollowStmt->close();
+
         $sailControlStmt = $this->db->stmt_init();
+        $sailControlStmt->prepare('UPDATE config_sail_control SET id=?, loop_time=?, max_sail_angle=?, min_sail_angle=?;');
+        $sailControlStmt->bind_param(NULL, 
+                                $data['']['loop_time'],
+                                $data['']['max_sail_angle'],
+                                $data['']['min_sail_angle']
+                            );
+        $sailControlStmt->execute();
+        $sailControlStmt->control();
+
         $simulatorStmt = $this->db->stmt_init();
+        $simulatorStmt->prepare('UPDATE config_simulator SET id=?, loop_time=?;');
+        $simulatorStmt->bind_param(NULL, $data['config_simulator']['loop_time']);
+        $simulatorStmt->execute();
+        $simulatorStmt->close();
+
         $solarTrackerStmt = $this->db->stmt_init();
+        $solarTrackerStmt->prepare('UPDATE config_solar_tracker SET id=?, loop_time=?;');
+        $solarTrackerStmt->bind_param(NULL, $data['config_solar_tracker']['loop_time']);
+        $solarTrackerStmt->execute();
+        $solarTrackerStmt->close();        
+
         $vesselStateStmt = $this->db->stmt_init();
+        $vesselStateStmt->prepare('UPDATE config_vessel_state SET id=?, loop_time=?, course_config_speed_1=?, course_config_speed_2=?;');
+        $vesselStateStmt->bind_param(NULL,
+                                    $data['config_vessel_state']['loop_time'],
+                                    $data['config_vessel_state']['course_config_speed_1'],
+                                    $data['config_vessel_state']['course_config_speed_2']
+                                );
+        $vesselStateStmt->execute();
+        $vesselStateStmt->close();
+
         $voterSystemStmt = $this->db->stmt_init();
-        $xbeeStmt = $this->db->stmt_init();
-        $currentMissionStmt = $this->db->stmt_init();
+        $voterSystemStmt->prepare('UPDATE config_voter_system SET id=?, loop_time=?, max_vote=?, waypoint_voter_weight=?, wind_voter_weight=?, channel_voter_weight=?, midrange_voter_weight=?, proximity_voter_weight=?;');
+        $voterSystemStmt->bind_param(NULL,
+                                    $data['config_voter_system']['loop_time'],
+                                    $data['config_voter_system']['max_vote'],
+                                    $data['config_voter_system']['waypoint_voter_weight'],
+                                    $data['config_voter_system']['wind_voter_weight'],
+                                    $data['config_voter_system']['channel_voter_weight'],
+                                    $data['config_voter_system']['midrange_voter_weight'],
+                                    $data['config_voter_system']['proximity_voter_weight']
+                                );
+        $voterSystemStmt->execute();
+        $voterSystemStmt->close();
 
-
-
-        // TODO: prepare stmt / execute / close
-        
-        $course_calculationStmt->prepare("UPDATE course_calculation_config SET sector_angle=?,tack_angle=?,tack_max_angle=?,tack_min_speed=? WHERE id=$id;");
-        $maestro_controllerStmt->prepare("UPDATE  maestro_controller_config SET port=? WHERE id=$id;");
-        $rudder_commandStmt->prepare("UPDATE rudder_command_config SET extreme_command=?,midship_command=? WHERE id=$id;");
-        $rudder_servoStmt->prepare("UPDATE rudder_servo_config SET acceleration=?, channel=?, speed=? WHERE id=$id;");
-        $sailing_robotStmt->prepare("UPDATE sailing_robot_config SET flag_heading_compass=?, loop_time=?, scanning=?, line_follow=?, require_network=? WHERE id=$id;");
-        $sail_commandStmt->prepare("UPDATE sail_command_config SET close_reach_command=?, run_command=? WHERE id=$id;");
-        $sail_servoStmt->prepare("UPDATE sail_servo_config SET acceleration=?,channel=?, speed=? WHERE id=$id;");
-        $waypoint_routingStmt->prepare("UPDATE waypoint_routing_config SET adjust_degree_limit=?, max_command_angle=?,radius_ratio=?, rudder_speed_min=?, sail_adjust_time=? WHERE id=$id;");
-        $windsensorStmt->prepare("UPDATE windsensor_config SET baud_rate=?, port=? WHERE id=$id");
-        $wind_vaneStmt->prepare("UPDATE wind_vane_config SET use_self_steering=?, wind_sensor_self_steering=?, self_steering_interval=? WHERE id=$id;");
-        $XbeeStmt->prepare("UPDATE xbee_config SET send=?, recieve=?, send_logs=?, delay=? WHERE id=$id;");
-        $HttpsyncStmt->prepare("UPDATE httpsync_config SET delay=? WHERE id=$id;");
-                $BufferStmt->prepare("UPDATE buffer_config SET compass=?, true_wind=?, windsensor=? WHERE id=$id;");
-
-        $course_calculationStmt->bind_param("dddd",$data["course_calculation_config"]["sector_angle"],
-        $data["course_calculation_config"]["tack_angle"],
-        $data["course_calculation_config"]["tack_max_angle"],
-        $data["course_calculation_config"]["tack_min_speed"]);
-        $course_calculationStmt->execute();
-
-        $maestro_controllerStmt->bind_param("s",$data["maestro_controller_config"]["port"]);
-        $maestro_controllerStmt->execute();
-
-        $rudder_commandStmt->bind_param("ii",$data["rudder_command_config"]["extreme_command"],
-        $data["rudder_command_config"]["midship_command"]);
-        $rudder_commandStmt->execute();
-
-        $rudder_servoStmt->bind_param("iii",$data["rudder_servo_config"]["acceleration"],
-        $data["rudder_servo_config"]["channel"],
-        $data["rudder_servo_config"]["speed"]);
-        $rudder_servoStmt->execute();
-
-        $sailing_robotStmt->bind_param("idiii", $data["sailing_robot_config"]["flag_heading_compass"],
-        $data["sailing_robot_config"]["loop_time"],
-        $data["sailing_robot_config"]["scanning"],
-        $data["sailing_robot_config"]["line_follow"],
-        $data["sailing_robot_config"]["require_network"]);
-        $sailing_robotStmt->execute();
-
-        $sail_commandStmt->bind_param("ii", $data["sail_command_config"]["close_reach_command"],
-        $data["sail_command_config"]["run_command"]);
-        $sail_commandStmt->execute();
-
-        $sail_servoStmt->bind_param("iii", $data["sail_servo_config"]["acceleration"],
-        $data["sail_servo_config"]["channel"],
-        $data["sail_servo_config"]["speed"]);
-        $sail_servoStmt->execute();
-
-        $waypoint_routingStmt->bind_param("ddddd", $data["waypoint_routing_config"]["adjust_degree_limit"],
-        $data["waypoint_routing_config"]["max_command_angle"],
-        $data["waypoint_routing_config"]["radius_ratio"],
-        $data["waypoint_routing_config"]["rudder_speed_min"],
-        $data["waypoint_routing_config"]["sail_adjust_time"]);
-        $waypoint_routingStmt->execute();
-
-        $windsensorStmt->bind_param("is", $data["windsensor_config"]["baud_rate"],
-        $data["windsensor_config"]["port"]);
+        $windSensorStmt = $this->db->stmt_init();
+        $windsensorStmt->prepare('UPDATE config_wind_sensor SET id=?, loop_time=?;');
+        $windsensorStmt->bind_param(NULL, $data['config_wind_sensor']['loop_time']);
         $windsensorStmt->execute();
+        $windsensorStmt->close();         
 
-        $wind_vaneStmt->bind_param("iid", $data["wind_vane_config"]["use_self_steering"],
-        $data["wind_vane_config"]["wind_sensor_self_steering"],
-        $data["wind_vane_config"]["self_steering_interval"]);
-        $wind_vaneStmt->execute();
+        $xbeeStmt = $this->db->stmt_init();
+        $xbeeStmt->prepare('UPDATE config_xbee SET id=?, loop_time=?, send=?, receive=?, send_logs=?, push_only_latest_logs=?;');
+        $xbeeStmt->bind_param(NULL, 
+                                $data['config_xbee']['loop_time'],
+                                $data['config_xbee']['send'],
+                                $data['config_xbee']['receive'],
+                                $data['config_xbee']['send_logs'],
+                                $data['config_xbee']['push_only_latest_logs']
+                            );
+        $xbeeStmt->execute();
+        $xbeeStmt->close();
 
-        $XbeeStmt->bind_param("iiii", $data["xbee_config"]["send"],
-        $data["xbee_config"]["recieve"],
-        $data["xbee_config"]["send_logs"],
-        $data["xbee_config"]["delay"]);
-        $XbeeStmt->execute();
+        $currentMissionStmt = $this->db->stmt_init();
+        $currentMissionStmt->prepare("UPDATE currentMission VALUES id=?, id_mission=?, rankInMission=?, isCheckpoint=?, name=?, latitude=?, longitude=?, declination=?, radius=?, stay_time=?, harvested=?;");
+        $currentMissionStmt->bind_param($data['current_mission']['id'],
+                                $data['current_mission']['id_mission'],
+                                $data['current_mission']['rankInMission'],
+                                $data['current_mission']['is_checkpoint'],
+                                $data['current_mission']['name'],
+                                $data['current_mission']['latitude'],
+                                $data['current_mission']['longitude'],
+                                $data['current_mission']['declination'],
+                                $data['current_mission']['radius'],
+                                $data['current_mission']['stay_time'],
+                                $data['current_mission']['harvested']
+                            );
+        $currentMissionStmt->execute();
+        $currentMissionStmt->close();
 
-        $HttpsyncStmt->bind_param("i", $data["httpsync_config"]["delay"]);
-        $HttpsyncStmt->execute();
-
-                $BufferStmt->bind_param("iii",$data["buffer_config"]["compass"],
-        $data["buffer_config"]["true_wind"],
-        $data["buffer_config"]["windsensor"]);
-        $BufferStmt->execute();
-
-
-        $course_calculationStmt->close();
-        $maestro_controllerStmt->close();
-        $rudder_commandStmt->close();
-        $rudder_servoStmt->close();
-        $sailing_robotStmt->close();
-        $sail_commandStmt->close();
-        $sail_servoStmt->close();
-        $waypoint_routingStmt->close();
-        $windsensorStmt->close();
-        $wind_vaneStmt->close();
-        $XbeeStmt->close();
-        $HttpsyncStmt->close();
-        $BufferStmt->close();
-
-        return $data["wind_vane_config"]["wind_sensor_self_steering"].$data["wind_vane_config"]["use_self_steering"];
+        //return $data["wind_vane_config"]["wind_sensor_self_steering"].$data["wind_vane_config"]["use_self_steering"];
         }
     }
 //when in non-wsdl mode the uri option must be specified
