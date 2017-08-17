55847a508d3f343a530685cbf7e8821a01548337
Antoine <frama.service@openaliasbox.org>
2017-08-17 09:33:07 +0200

    uncommented variable

--- globalsettings.php
+++ globalsettings.php
@@ -15,7 +15,7 @@
 
     ob_start(); // Allows the session header to be cached
 
-    /* $GLOBALS['server'] = "http://localhost/tests/Tuto_OCR/SailingRobotsWebsite/"; */
+    $GLOBALS['server'] = "http://localhost/tests/Tuto_OCR/SailingRobotsWebsite/"; 
     //$GLOBALS['server'] = "http://www.sailingrobots.com/testdata/";
     $GLOBALS['username'] = 'root';
     //$GLOBALS['username'] = 'ithaax_testdata';
