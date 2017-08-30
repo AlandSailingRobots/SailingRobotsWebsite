55a636c54e421ff9b0caf07ed626dd2b6dcb340f
Antoine <frama.service@openaliasbox.org>
2017-08-30 12:16:19 +0200

    simplified code for db modif

--- pages/configuration/sailingRobot/admin_body.php
+++ pages/configuration/sailingRobot/admin_body.php
@@ -17,88 +17,62 @@ require_once('include/getConfigData.php');
         <form action='updateConfig_post.php' method='post'>
             <div class="row">
                 <?php
+
                 if (isset($_GET['boat']) && $_GET['boat'] == "janet")
                 {
+                    $allowed_query = array (0 => 'buffer_config',
+                        1 => 'course_calculation_config',
+                        2 => 'maestro_controller_config',
+                        3 => 'rudder_command_config',
+                        4 => 'rudder_servo_config',
+                        5 => 'sail_command_config',
+                        6 => 'sail_servo_config',
+                        7 => 'sailing_robot_config',
+                        8 => 'waypoint_routing_config',
+                        9 => 'wind_vane_config',
+                        10 => 'windsensor_config',
+                        11 => 'xbee_config',
+                        12 => 'httpsync_config'
+                    );
                     echo '<input type="text" class="hidden" name="boat|janet" value="janet" size="1">';
                     // We get every table from the database
-                    $bufferConfigArray            = getConfigData("buffer_config"             , "janet");
-                    $courseCalculationConfigArray = getConfigData("course_calculation_config" , "janet");
-                    $maestroControllerConfigArray = getConfigData("maestro_controller_config" , "janet");
-                    $rudderCommandConfigArray     = getConfigData("rudder_command_config"     , "janet");
-                    $rudderServoConfigArray       = getConfigData("rudder_servo_config"       , "janet");
-                    $sailCommandConfigArray       = getConfigData("sail_command_config"       , "janet");
-                    $sailServoConfigArray         = getConfigData("sail_servo_config"         , "janet");
-                    $sailingRobotConfigArray      = getConfigData("sailing_robot_config"      , "janet");
-                    $waypointRoutingConfigArray   = getConfigData("waypoint_routing_config"   , "janet");
-                    $windVaneConfigArray          = getConfigData("wind_vane_config"          , "janet");
-                    $windsensorConfigArray        = getConfigData("windsensor_config"         , "janet");
-                    $xbeeConfigArray              = getConfigData("xbee_config"               , "janet");
-                    $httpSyncConfigArray          = getConfigData("httpsync_config"           , "janet");
-
-                    // We display the different tables
-                    printTables($bufferConfigArray            , "buffer_config");
-                    printTables($courseCalculationConfigArray , "course_calculation_config");
-                    printTables($maestroControllerConfigArray , "maestro_controller_config");
-
-                    printTables($rudderCommandConfigArray , "rudder_command_config");
-                    printTables($rudderServoConfigArray   , "rudder_servo_config");
-                    printTables($sailCommandConfigArray   , "sail_command_config");
                     
-                    printTables($sailServoConfigArray       , "sail_servo_config");
-                    printTables($sailingRobotConfigArray    , "sailing_robot_config");
-                    printTables($waypointRoutingConfigArray , "waypoint_routing_config");
-                    
-                    printTables($windVaneConfigArray   , "wind_vane_config");
-                    printTables($windsensorConfigArray , "windsensor_config");
-                    printTables($httpSyncConfigArray   , "httpsync_config");
-                    printTables($xbeeConfigArray       , "xbee_config");
+                    foreach ($allowed_query as $key => $value) 
+                    {
+                        $valueArray = getConfigData($value, "janet");
+                        printTables($valueArray, $value);
+                    }
+
                 }
                 else if (isset($_GET['boat']) && $_GET['boat'] == "aspire")
                 {
+                    $allowed_query = array (0 => 'config_buffer',
+                        1 => 'config_course_regulator',
+                        2 => 'config_dblogger',
+                        3 => 'config_gps',
+                        4 => 'config_i2c',
+                        5 => 'config_httpsync',
+                        6 => 'config_marine_sensors',
+                        7 => 'config_simulator',
+                        8 => 'config_solar_tracker',
+                        9 => 'config_vessel_state',
+                       10 => 'config_voter_system',
+                       11 => 'config_wind_sensor',
+                       12 => 'config_xbee',
+                       13 => 'config_ais',
+                       14 => 'config_ais_processing',
+                       15 => 'config_can_arduino',
+                       16 => 'config_line_follow',
+                       17 => 'config_sail_control'
+                    );
+
                     echo '<input type="text" class="hidden" name="boat|aspire" value="aspire" size="1">';
                     
-                    $configAISArray              = getConfigData("config_ais"                 , "aspire");
-                    $configAISProcessingArray    = getConfigData("config_ais_processing"      , "aspire");
-                    $configCanArduinoArray       = getConfigData("config_can_arduino"         , "aspire");
-                    $configBufferArray           = getConfigData("config_buffer"              , "aspire");
-                    $configCourseRegulatorArray  = getConfigData("config_course_regulator"    , "aspire");
-                    $configDBLoggerArray         = getConfigData("config_dblogger"            , "aspire");
-                    $configGPSArray              = getConfigData("config_gps"                 , "aspire");
-                    $configHTTPSyncArray         = getConfigData("config_httpsync"            , "aspire");
-                    $configI2CArray              = getConfigData("config_i2c"                 , "aspire");
-                    $configMarineSensorsArray    = getConfigData("config_marine_sensors"      , "aspire");
-                    $configSimulatorArray        = getConfigData("config_simulator"           , "aspire");
-                    
-                    $configSolarTrackerArray     = getConfigData("config_solar_tracker"       , "aspire");
-                    $configVesselStateArray      = getConfigData("config_vessel_state"        , "aspire");
-                    $configVoterSystemArray      = getConfigData("config_voter_system"        , "aspire");
-                    $configWindSensorArray       = getConfigData("config_wind_sensor"         , "aspire");
-                    $configXBeeArray             = getConfigData("config_xbee"                , "aspire");
-
-                    $configLineFollowArray       = getConfigData("config_line_follow"         , "aspire");
-                    $configSailControlArray      = getConfigData("config_sail_control"        , "aspire");
-
-                    // We display the different tables
-                    printTables($configAISArray             , "config_ais");
-                    printTables($configAISProcessingArray   , "config_ais_processing");
-                    printTables($configCanArduinoArray      , "config_can_arduino");
-                    printTables($configBufferArray          , "config_buffer");
-                    printTables($configCourseRegulatorArray , "config_course_regulator");
-                    printTables($configDBLoggerArray        , "config_dblogger");
-                    printTables($configGPSArray             , "config_gps");
-                    printTables($configI2CArray             , "config_i2c");
-                    printTables($configMarineSensorsArray   , "config_marine_sensors");
-                    printTables($configSimulatorArray       , "config_simulator");
-
-                    printTables($configSolarTrackerArray    , "config_solar_tracker");
-                    printTables($configVesselStateArray     , "config_vessel_state");
-                    printTables($configWindSensorArray      , "config_wind_sensor");
-                    printTables($configVoterSystemArray     , "config_voter_system");
-                    printTables($configHTTPSyncArray        , "config_httpsync");
-                    printTables($configXBeeArray            , "config_xbee");
-
-                    printTables($configLineFollowArray      , "config_line_follow");
-                    printTables($configSailControlArray     , "config_sail_control");
+                    foreach ($allowed_query as $key => $value) 
+                    {
+                        $valueArray = getConfigData($value, "aspire");
+                        printTables($valueArray, $value);
+                    }
 
                 }
                 ?>
--- pages/configuration/sailingRobot/include/getConfigData.php
+++ pages/configuration/sailingRobot/include/getConfigData.php
@@ -8,43 +8,10 @@ function getConfigData($table, $boatName)
     if ($boatName == "janet")
     {
         $database_name = $GLOBALS['database_name_testdata'];
-        $allowed_query = array (0 => 'buffer_config',
-                                1 => 'course_calculation_config',
-                                2 => 'maestro_controller_config',
-                                3 => 'rudder_command_config',
-                                4 => 'rudder_servo_config',
-                                5 => 'sail_command_config',
-                                6 => 'sail_servo_config',
-                                7 => 'sailing_robot_config',
-                                8 => 'waypoint_routing_config',
-                                9 => 'wind_vane_config',
-                                10 => 'windsensor_config',
-                                11 => 'xbee_config',
-                                12 => 'httpsync_config'
-                            );
     }
     elseif ($boatName == "aspire") 
     {
         $database_name = $GLOBALS['database_ASPire'];
-        $allowed_query = array (0 => 'config_buffer',
-                                1 => 'config_course_regulator',
-                                2 => 'config_dblogger',
-                                3 => 'config_gps',
-                                4 => 'config_i2c',
-                                5 => 'config_httpsync',
-                                6 => 'config_marine_sensors',
-                                7 => 'config_simulator',
-                                8 => 'config_solar_tracker',
-                                9 => 'config_vessel_state',
-                               10 => 'config_voter_system',
-                               11 => 'config_wind_sensor',
-                               12 => 'config_xbee',
-                               13 => 'config_ais',
-                               14 => 'config_ais_processing',
-                               15 => 'config_can_arduino',
-                               16 => 'config_line_follow',
-                               17 => 'config_sail_control'
-                            );
     }
 
     $user          = $GLOBALS['username'];
@@ -65,21 +32,14 @@ function getConfigData($table, $boatName)
         die('Connection failed : '.$e->getMessage());
     }
 
-    // I tried to protect the sql query -- Don't know if it's necessary
-
-    if (in_array($table, $allowed_query, true))
-    {
-        $index = array_search($table, $allowed_query);
-        $protected_table = $allowed_query[$index];
+    // SQL Query
+    $req = $db->prepare('SELECT * FROM ' . $table);
+    $req->execute();
+    $result = $req->fetch(PDO::FETCH_ASSOC);
+    $req->closeCursor();
     
-        // SQL Query
-        $req = $db->prepare('SELECT * FROM ' . $protected_table);
-        $req->execute();
-        $result = $req->fetch(PDO::FETCH_ASSOC);
-        $req->closeCursor();
-        
-        return ($result);
-    }
+    return ($result);
+
 
 
 }
