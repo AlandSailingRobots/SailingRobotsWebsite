77cf98d259357a652d6dc5d2f1e0f370bb108c3e
Kåre Hampf <kare.hampf@ha.ax>
2018-05-02 16:21:32 +0300

    Cleaned up formatting

--- pages/data/measurements/php/measurements.php
+++ pages/data/measurements/php/measurements.php
@@ -1,22 +1,28 @@
 <?php
-Class Measurements {
+Class Measurements
+{
 
-    public function __construct() {
-
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
     }
 
-    public function prepare() {
-
-        if ($this->boatName == "janet")
-        {
+    /**
+     * Prepare
+     *
+     * @return void
+     **/
+    public function prepare()
+    {
+        if ($this->boatName == "janet") {
             $database_name = $GLOBALS['database_name_testdata'];
-        }
-        elseif ($this->boatName == "aspire")
-        {
+        } else if ($this->boatName == "aspire") {
             $database_name = $GLOBALS['database_ASPire'];
         }
 
-        #SETUP
+        // SETUP
         $this->host     = $GLOBALS['hostname'];
         $this->db       = $database_name;
         $this->usr      = $GLOBALS['username'];
@@ -32,23 +38,30 @@ Class Measurements {
 
         $this->dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
 
-        $this->fetch_table = ' (SELECT * FROM ithaax_ASPire_config.dataLogs_system) AS mission_dataLogs ';
-        $this->fetch_table .='JOIN ithaax_ASPire_config.dataLogs_marine_sensors ';
-        $this->fetch_table .='ON mission_dataLogs.marine_sensors_id = dataLogs_marine_sensors.id ';
-        $this->fetch_table .='JOIN ithaax_ASPire_config.dataLogs_gps ';
-        $this->fetch_table .='ON mission_dataLogs.gps_id = dataLogs_gps.id ';
-        $this->fetch_table .='JOIN ithaax_ASPire_config.currentMission ';
-        $this->fetch_table .='ON mission_dataLogs.current_mission_id = currentMission.id ';
-        $this->fetch_table .='JOIN ithaax_mission.mission ';
-        $this->fetch_table .='ON ithaax_ASPire_config.currentMission.id_mission = mission.id ';
+        $this->fetch_table
+            = '(SELECT * FROM ithaax_ASPire_config.dataLogs_system) AS mission_dataLogs'
+            .' JOIN ithaax_ASPire_config.dataLogs_marine_sensors'
+            .' ON mission_dataLogs.marine_sensors_id = dataLogs_marine_sensors.id'
+            .' JOIN ithaax_ASPire_config.dataLogs_gps'
+            .' ON mission_dataLogs.gps_id = dataLogs_gps.id'
+            .' JOIN ithaax_ASPire_config.currentMission'
+            .' ON mission_dataLogs.current_mission_id = currentMission.id'
+            .' JOIN ithaax_mission.mission'
+            .' ON ithaax_ASPire_config.currentMission.id_mission = mission.id';
     }
 
-    # Counts nr of pages
+    /**
+     * Counts number of pages
+     *
+     * @param string $table Table name
+     *
+     * @return int Number of pages
+     **/
     function getPages($table)
     {
         $pdo = new PDO($this->dsn, $this->usr, $this->pwd, $this->opt);
 
-        $total = $pdo->query("SELECT COUNT(*) as rows FROM $table ;") ->fetch(PDO::FETCH_OBJ);
+        $total = $pdo->query("SELECT COUNT(*) as rows FROM $table ;")->fetch(PDO::FETCH_OBJ);
         $perpage = $this->limit;
         $posts   = $total->rows;
         $pages   = ceil($posts / $perpage);
@@ -56,7 +69,7 @@ Class Measurements {
         return $pages;
     }
 
-    # Writes the colum name from DB
+    // Writes the colum name from DB
     public function getColumnNames($sqlResult) {
         $someHTMLString = '';
 
@@ -70,7 +83,7 @@ Class Measurements {
         return $someHTMLString;
     }
 
-    # Writes the colum data
+    // Writes the colum data
     public function getColumnData($sqlResult) {
         $someHTMLString = '';
 
@@ -86,7 +99,7 @@ Class Measurements {
         return $someHTMLString;
     }
 
-    # building the SQL query
+    // building the SQL query
     public function getSensorLogData($offset, $limit) {
         $pdo = new PDO($this->dsn, $this->usr, $this->pwd, $this->opt);
         $pdo->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
@@ -119,7 +132,7 @@ Class Measurements {
         return $this->$name;
     }
 
-    # Generate table data as html for template
+    // Generate table data as html for template
     public function __toString() {
         $someHTMLString = null;
         $sqlResult = $this->getSensorLogData($this->offset, $this->limit);
@@ -135,25 +148,24 @@ Class Pager {
     public function __construct($numPages) {
         $this->pages = $numPages;
     }
-
+    
     public function __set($name, $value) {
         $this->$name = $value;
     }
-
+    
     public function __get($name) {
         return $this->$name;
     }
-
-
+    
     public function __toString() {
         $firstPage = 1;
         $midRange = 5;
         $lastPage = $this->pages;
-
+    
         $pagerLinks = null;
         $pagerLinks .= '<ul class="pagination">';
-
-        #check previous
+    
+        // check previous
         if($this->currentPage == 1) {
             $pagerLinks .= '<li class="disabled"><a href="#"><<</a></li>';
             $pagerLinks .= '<li class="disabled"><a href="#"><</a></li>';
@@ -162,7 +174,7 @@ Class Pager {
             $pagerLinks .= '<li><a href="index.php?page='.$firstPage.'"><<</a></li>';
             $pagerLinks .= '<li><a href="index.php?page='.$prevPage.'"><</a></li>';
         }
-
+    
         $startRange;
         $stopRange;
         if($this->currentPage < $midRange) {
@@ -177,10 +189,10 @@ Class Pager {
             $startRange = $this->pages - 9;
             $stopRange = $this->pages;
         }
-
-        #pages
+    
+        // pages
         for ($x = 0; $x < $this->pagesToShow; $x++) {
-
+    
             $pageNum = $x + $startRange;
             if ($pageNum == $this->currentPage) {
                 $pagerLinks .= '<li class="active"><a href=#">'.$pageNum.'</a></li>';
@@ -188,8 +200,8 @@ Class Pager {
                 $pagerLinks .= '<li><a href="index.php?page='.$pageNum.'">'.$pageNum.'</a></li>';
             }
         }
-
-        #check next
+    
+        // check next
         if($this->currentPage == $this->pages) {
             $pagerLinks .= '<li class="disabled"><a href="#">></a></li>';
             $pagerLinks .= '<li class="disabled"><a href="#">>></a></li>';
@@ -199,7 +211,7 @@ Class Pager {
             $pagerLinks .= '<li><a href="index.php?page='.$lastPage.'">>></a></li>';
         }
         $pagerLinks .= '</ul>';
-
+    
         return $pagerLinks;
     }
 }
