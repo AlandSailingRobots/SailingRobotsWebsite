e4403f4388e5568349846fc69fdc341b99220e87
Jan Jakobsson <jan.jakobsson@ha.ax>
2018-07-19 16:25:24 +0300

    fixed indentation

--- pages/logs/live/admin_body.php
+++ pages/logs/live/admin_body.php
@@ -6,14 +6,11 @@
 
 <div class="container-fluid">
     <div class="row">
-
         <div class="col-sm-6">
             <h2>Current Sensors</h2>
             <div id="currentSensorDataKey" class="liveData col-sm-3"></div>
             <div id="currentSensorDataValue" class="liveData col-sm-3"></div>
         </div>
-
-
         <div class="col-sm-6">
             <h2>GPS Data</h2>
             <div id="gpsDataKey" class="liveData col-sm-3"></div>
@@ -22,14 +19,11 @@
     </div>
 
     <div class="row">
-
         <div class="col-sm-6">
             <h2>Compass Data</h2>
             <div id="compassDataKey" class="liveData col-sm-3"></div>
             <div id="compassDataValue" class="liveData col-sm-3"></div>
         </div>
-
-
         <div class="col-sm-6">
             <h2>Wind Sensor Data</h2>
             <div id="windSensorDataKey" class="liveData col-sm-3"></div>
@@ -38,29 +32,20 @@
     </div>
 
     <div class="row">
-
         <div class="col-sm-6">
             <h2>Course Data</h2>
             <div id="courseDataKey" class="liveData col-sm-3"></div>
             <div id="courseDataValue" class="liveData col-sm-3"></div>
         </div>
-
-
         <div class="col-sm-6">
             <h2>Marine Sensor Data</h2>
-            <div id="marineSensorDataKey" class="liveData col-sm-3">
-
-            </div>
-            <div id="marineSensorDataValue" class="liveData col-sm-3">
-
-            </div>
+            <div id="marineSensorDataKey" class="liveData col-sm-3"></div>
+            <div id="marineSensorDataValue" class="liveData col-sm-3"></div>
         </div>
     </div>
 </div>
 
 <div class="container">
-
-
     <div class = "mapClass">
         <div id="map"></div>
         <div id="legend"></div>
