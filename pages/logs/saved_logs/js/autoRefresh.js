d225b7e905b40b9b4d01af3acf6014af07a6901b
Jan Jakobsson <jan.jakobsson@ha.ax>
2018-07-20 16:14:09 +0300

    Should now work as intended with session cookie Defaults to 6 seconds

--- pages/logs/saved_logs/js/autoRefresh.js
+++ pages/logs/saved_logs/js/autoRefresh.js
@@ -1,45 +1,92 @@
-let enabled = true;
-let defaultTimeout = 50000;
-let timeout = defaultTimeout
+const count = 1;
+let enabled = null;
+let timeout = 6000;
+let refreshTimer;
 
 
-setCookie("timeout", timeout);
-setCookie("enabled", true);
+$(document).ready(function () {
+    enabled = JSON.parse(getCookie("enabled"));
+    init();
+    updateButton();
 
-//JSON.parse("true"); //returns boolean true
+});
+
+function init () {
+    if (document.cookie.indexOf("enabled") == -1 ) {
+        setCookie("timeout", timeout);
+        setCookie("enabled", true);
+        setCookie("counter", 1);
+        document.getElementById ("timeoutBtn").innerText = "Auto refresh [init = ON]";
+    }
+    updateButton();
+    runTimer(enabled);
+
+    //======= DEBUG =========
+    console.log("current cookie: ");
+    console.log(document.cookie);
+    counter = JSON.parse(getCookie("counter"));
+    counter += count;
+    setCookie("counter", counter);
+    //=======================
+
+}
+
+function runTimer (bool) {
+    window.onload = function () {
+        if (bool == true) {
+            refreshTimer = startTimer();
+        } else {
+            stopTimer(refreshTimer);
+        }
+    }
+}
+
+function startTimer () {
+    let timer = setInterval(function () {
+        window.location.reload();
+    }, 3000);
+
+    return timer;
+}
+
+function stopTimer (timer) {
+    window.clearInterval(timer);
+}
 
 function setCookie (name, value) {
     document.cookie = name + "=" + value + ";";
 }
 
 function getCookie(name) {
-    var value = "; " + document.cookie;
-    var parts = value.split("; " + name + "=");
+    let value = "; " + document.cookie;
+    let parts = value.split("; " + name + "=");
     if (parts.length == 2) return parts.pop().split(";").shift();
 }
-
-console.log(getCookie("timeout"));
-console.log(getCookie("enabled"));
-console.log(document.cookie);
-
 
 document.getElementById ("timeoutBtn").onclick = function () {
     toggleTimeout ();
 };
 
-setTimeout(function () {
-    window.location.reload();
-}, timeout);
-
 function toggleTimeout () {
+    enabled = JSON.parse(getCookie("enabled"));
 
-    if (enabled) {
-        enabled = false;
-        document.getElementById ("timeoutBtn").innerText = "Auto refresh [OFF]";
-        console.log("OFF");
-    } else {
-        enabled = true;
+        if (enabled) {
+            stopTimer(refreshTimer);
+            setCookie("enabled", false);
+            enabled = false;
+            updateButton();
+        } else {
+            setCookie("enabled", true);
+            enabled = true;
+            updateButton();
+            refreshTimer = startTimer(timeout);
+        }
+}
+
+function updateButton () {
+    if (enabled == true) {
         document.getElementById ("timeoutBtn").innerText = "Auto refresh [ON]";
-        console.log("ON");
+    } else {
+        document.getElementById ("timeoutBtn").innerText = "Auto refresh [OFF]";
     }
 }
