682224d92d5f9eaadec8f565e6160de1e2d0890d
Jan Jakobsson <jan.jakobsson@ha.ax>
2018-08-01 10:51:24 +0300

    support for ASPire and Janet

--- include/database/DatabaseConnectionFactory.php
+++ include/database/DatabaseConnectionFactory.php
@@ -5,34 +5,52 @@
  * Date: 7/26/18
  * Time: 1:24 PM
  */
+require_once(__ROOT__.'/globalsettings.php');
 require_once ('AbstractDatabaseConnectionFactory.php');
 
 class DatabaseConnectionFactory extends AbstractDatabaseConnectionFactory {
+
+    /**
+     * DatabaseConnectionFactory constructor.
+     */
+    private function __construct() {}
 
     /**
      * @param string $boatName
      * @return DatabaseConnection
      */
     static function getDatabaseConnection (string $boatName): DatabaseConnection {
-        $host     = "localhost";
+        $host     =  $GLOBALS['hostname'];
         $port     = "3306";
-        $db       = "ithaax_ASPire_config";
-        $usr      = "root";
-        $pwd      = "";
+        $db       = self::getDb($boatName);
+        $usr      = $GLOBALS['username'];
+        $pwd      = $GLOBALS['password'];
         $charset  = 'utf8mb4';
         $opt = [PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
         ];
-
-        $config;
-        $connection;
-        $handler;
-
-
+        
         $config = new DatabaseConfiguration($host, $port, $db, $usr, $pwd, $charset, $opt);
         $connection = new DatabaseConnection($config);
 
         return $connection;
+    }
+
+    /**
+     * @param string $boatName
+     * @return string
+     */
+    private static function getDb(string $boatName) {
+        $checkBoatName = strtolower($boatName);
+
+        switch ($checkBoatName) {
+            case "aspire":
+                return $GLOBALS['database_ASPire'];
+            case "janet":
+                return $GLOBALS['database_name_testdata'];
+            default:
+                return $GLOBALS['database_ASPire'];
+        }
     }
 }
